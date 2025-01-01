6f9f690f163a24dd152dab946abb2865f5aac8e7
MD MAHABUB ALAM <mahabubx7@gmail.com>
2025-01-01 05:34:52 +0000

    chore: added solution

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// api/whoami
+app.get('/api/whoami', (req, res) => {
+  return res.json({
+    ipaddress: req.ip,
+    language: req.headers['content-language'] || req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
